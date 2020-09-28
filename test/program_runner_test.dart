21826f3f5b1cb74ff28abc72c3568a019f976c20
Skycoder42 <Skycoder42@users.noreply.github.com>
2020-09-28 07:53:33 +0200

    fix windows CI

--- test/program_runner_test.dart
+++ test/program_runner_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dart_lint_hooks/src/logger.dart';
 import 'package:dart_lint_hooks/src/program_runner.dart';
 import 'package:dart_lint_hooks/src/task_error.dart';
@@ -17,24 +19,30 @@ void main() {
     sut = ProgramRunner(mockLogger);
   });
 
+  Future<int> _run(List<String> args) => Platform.isWindows
+      ? sut.run("cmd", ["/c", ...args])
+      : sut.run("bash", ["-c", ...args]);
+
+  Stream<String> _stream(List<String> args) => Platform.isWindows
+      ? sut.stream("cmd", ["/c", ...args])
+      : sut.stream("bash", ["-c", ...args]);
+
   test("run forwards exit code", () async {
-    final exitCode = await sut.run("bash", const ["-c", "exit 42"]);
+    final exitCode = await _run(const ["exit 42"]);
     expect(exitCode, 42);
   });
 
   group("stream", () {
     test("forwards output", () async {
-      final res = await sut.stream("bash", const [
-        "-c",
-        "echo a ; echo b ; echo c",
-      ]).toList();
+      final res = await _stream(const [
+        "echo a && echo b && echo c",
+      ]).map((e) => e.trim()).toList();
       expect(res, const ["a", "b", "c"]);
     });
 
     test("throws error if exit code indicates so", () async {
-      final stream = sut.stream("bash", const [
-        "-c",
-        "echo a ; echo b; false",
+      final stream = _stream(const [
+        "echo a && echo b && false",
       ]);
       expect(() => stream.last, throwsA(isA<TaskError>()));
     });
