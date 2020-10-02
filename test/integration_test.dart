a4fcd170e69971eb8546e30db65c4afc831ffcf9
Skycoder42 <Skycoder42@users.noreply.github.com>
2020-10-02 08:31:26 +0200

    try fix CI

--- test/integration_test.dart
+++ test/integration_test.dart
@@ -4,10 +4,61 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dart_pre_commit/src/hooks.dart';
+import 'package:path/path.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final cwd = Directory.current;
+  Directory testDir;
+
+  Future<void> _writeFile(String path, String contents) async {
+    final file = File(join(testDir.path, path));
+    if (!await file.parent.exists()) {
+      await file.parent.create(recursive: true);
+    }
+    await file.writeAsString(contents);
+  }
+
+  Future<String> _readFile(String path) =>
+      File(join(testDir.path, path)).readAsString();
+
+  Future<int> _run(
+    String program,
+    List<String> arguments, {
+    bool failOnError = true,
+    Function(Stream<List<int>>) onStdout,
+  }) async {
+    print("\$ $program ${arguments.join(" ")}");
+    final proc = await Process.start(
+      program,
+      arguments,
+      workingDirectory: testDir.path,
+    );
+    if (onStdout != null) {
+      onStdout(proc.stdout);
+    } else {
+      proc.stdout.listen(stdout.add);
+    }
+    proc.stderr.listen(stderr.add);
+    final exitCode = await proc.exitCode;
+    if (failOnError && exitCode != 0) {
+      throw "Failed to run '$program ${arguments.join(" ")}' with exit code: $exitCode";
+    }
+    return exitCode;
+  }
+
+  Future<void> _git(List<String> arguments) async => _run("git", arguments);
+
+  Future<int> _pub(
+    List<String> arguments, {
+    bool failOnError = true,
+    Function(Stream<List<int>>) onStdout,
+  }) async =>
+      _run(
+        Platform.isWindows ? "pub.bat" : "pub",
+        arguments,
+        failOnError: failOnError,
+        onStdout: onStdout,
+      );
 
   Future<int> _sut(
     List<String> arguments, {
@@ -22,11 +73,11 @@ void main() {
 
   setUp(() async {
     // create git repo
-    Directory.current = await Directory.systemTemp.createTemp();
+    testDir = await Directory.systemTemp.createTemp();
     await _git(const ["init"]);
 
     // create files
-    await File("pubspec.yaml").writeAsString("""
+    await _writeFile("pubspec.yaml", """
 name: test_project
 version: 0.0.1
 
@@ -36,13 +87,13 @@ environment:
 dependencies:
   meta: null
   dart_pre_commit:
-    path: ${cwd.path}
+    path: ${Directory.current.path}
 
 dev_dependencies:
   lint: null
 """);
-    await Directory("lib/src").create(recursive: true);
-    await File("lib/src/fix_imports.dart").writeAsString("""
+
+    await _writeFile("lib/src/fix_imports.dart", """
 // this is important
 import "package:test_project/test_project.dart";
 import "dart:io";
@@ -50,15 +101,14 @@ import "package:stuff/stuff.dart";
 
 void main() {}
 """);
-    await Directory("bin").create(recursive: true);
-    await File("bin/format.dart").writeAsString("""
+    await _writeFile("bin/format.dart", """
 import "package:test_project/test_project.dart";
 
 void main() {
   final x = "this is a very very very very very very very very very very very very very very very very very very very very very very long string";
 }
 """);
-    await File("lib/src/analyze.dart").writeAsString("""
+    await _writeFile("lib/src/analyze.dart", """
 void main() {
   var x = "constant";
 }
@@ -69,16 +119,14 @@ void main() {
   });
 
   tearDown(() async {
-    final tDir = Directory.current;
-    Directory.current = cwd;
-    await tDir.delete(recursive: true);
+    await testDir.delete(recursive: true);
   });
 
   test('fix imports', () async {
     await _git(const ["add", "lib/src/fix_imports.dart"]);
     await _sut(const ["--no-analyze"]);
 
-    final data = await File("lib/src/fix_imports.dart").readAsString();
+    final data = await _readFile("lib/src/fix_imports.dart");
     expect(data, """
 // this is important
 import "dart:io";
@@ -95,7 +143,7 @@ void main() {}
     await _git(const ["add", "bin/format.dart"]);
     await _sut(const ["--no-analyze"]);
 
-    final data = await File("bin/format.dart").readAsString();
+    final data = await _readFile("bin/format.dart");
     expect(data, """
 import "package:test_project/test_project.dart";
 
@@ -124,40 +172,3 @@ void main() {
         "  hint - The value of the local variable 'x' isn't used. - lib/src/analyze.dart:2:7 - unused_local_variable");
   });
 }
-
-Future<int> _run(
-  String program,
-  List<String> arguments, {
-  Directory cwd,
-  bool failOnError = true,
-  Function(Stream<List<int>>) onStdout,
-}) async {
-  print("\$ $program ${arguments.join(" ")}");
-  final proc =
-      await Process.start(program, arguments, workingDirectory: cwd?.path);
-  if (onStdout != null) {
-    onStdout(proc.stdout);
-  } else {
-    proc.stdout.listen(stdout.add);
-  }
-  proc.stderr.listen(stderr.add);
-  final exitCode = await proc.exitCode;
-  if (failOnError && exitCode != 0) {
-    throw "Failed to run '$program ${arguments.join(" ")}' with exit code: $exitCode";
-  }
-  return exitCode;
-}
-
-Future<void> _git(List<String> arguments) async => _run("git", arguments);
-
-Future<int> _pub(
-  List<String> arguments, {
-  bool failOnError = true,
-  Function(Stream<List<int>>) onStdout,
-}) async =>
-    _run(
-      Platform.isWindows ? "pub.bat" : "pub",
-      arguments,
-      failOnError: failOnError,
-      onStdout: onStdout,
-    );
