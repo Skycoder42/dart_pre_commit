c58fc0ef1e6caed063cecdf35a0a008b8eb391a0
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-02-03 17:11:58 +0100

    add test import task with tests

---
+++ lib/src/tasks/test_import_task.dart
@@ -0,0 +1,72 @@
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:dart_test_tools/dart_test_tools.dart';
+import 'package:path/path.dart';
+
+import '../../dart_pre_commit.dart';
+
+typedef AnalysisContextCollectionProviderFn = AnalysisContextCollection
+    Function(RepoEntry entry);
+
+class TestImportException implements Exception {
+  final String message;
+
+  TestImportException(this.message);
+
+  @override
+  String toString() => message;
+}
+
+class TestImportTask implements FileTask {
+  /// The [AnalysisContextCollectionProviderFn] used by this task.
+  final AnalysisContextCollectionProviderFn analysisContextCollectionProvider;
+
+  /// The [TaskLogger] instance used by this task.
+  final TaskLogger logger;
+
+  /// The [TestImportLinter] used by this task.
+  final TestImportLinter linter;
+
+  @override
+  String get taskName => 'test-imports';
+
+  const TestImportTask({
+    required this.analysisContextCollectionProvider,
+    required this.logger,
+    required this.linter,
+  });
+
+  @override
+  bool canProcess(RepoEntry entry) {
+    try {
+      linter.contextCollection = analysisContextCollectionProvider(entry);
+      return linter.shouldAnalyze(_normalizedAbsolutePath(entry));
+      // ignore: avoid_catching_errors
+    } on StateError {
+      return false;
+    }
+  }
+
+  @override
+  Future<TaskResult> call(RepoEntry entry) async {
+    linter.contextCollection = analysisContextCollectionProvider(entry);
+    final result = await linter.analyzeFile(_normalizedAbsolutePath(entry));
+    return result.when(
+      accepted: (_) => TaskResult.accepted,
+      rejected: (reason, resultLocation) {
+        logger.error(reason);
+        return TaskResult.rejected;
+      },
+      skipped: (reason, _) {
+        logger.info(reason);
+        return TaskResult.accepted;
+      },
+      failure: (error, stackTrace, _) {
+        logger.except(TestImportException(error), stackTrace);
+        return TaskResult.rejected;
+      },
+    );
+  }
+
+  String _normalizedAbsolutePath(RepoEntry entry) =>
+      normalize(entry.file.absolute.path);
+}
--- test/unit/global_mocks.dart
+++ test/unit/global_mocks.dart
@@ -2,12 +2,16 @@ import 'dart:io';
 
 import 'package:dart_pre_commit/src/repo_entry.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart' as p;
 
 class FakeFile extends Fake implements File {
   final bool _exists;
 
   @override
   final String path;
+
+  @override
+  File get absolute => FakeFile(p.absolute(path), exists: _exists);
 
   @override
   Future<bool> exists() async => _exists;
