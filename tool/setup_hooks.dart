7b296fb93e851e567f2022c12d9ee231c14ff4a4
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-10-05 15:30:35 +0200

    remove unused check script

---
+++ tool/setup_hooks.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final preCommitHook = File('.git/hooks/pre-commit');
+  await preCommitHook.parent.create(recursive: true);
+  await preCommitHook.writeAsString(
+    '''
+#!/bin/bash
+exec dart run bin/dart_pre_commit.dart --continue-on-rejected
+''',
+  );
+
+  if (!Platform.isWindows) {
+    final result = await Process.run('chmod', ['a+x', preCommitHook.path]);
+    stdout.write(result.stdout);
+    stderr.write(result.stderr);
+    exitCode = result.exitCode;
+  }
+}
