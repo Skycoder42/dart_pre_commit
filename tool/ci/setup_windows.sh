92d37ca19f172474e4818afb6c1afd3e751f00eb
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-03-11 15:10:48 +0000

    ....

--- tool/ci/setup_windows.sh
+++ tool/ci/setup_windows.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-set -exo pipefail
+set -eo pipefail
 
 echo "::group::Install OSV-Scanner"
-osv_version=$(git ls-remote --tags https://github.com/google/osv-scanner.git | grep -v "-" | cut -d 'v' -f2 | tail -n1)
-osv_dir=$RUNNER_TOOL_CACHE\\osv-scanner
-mkdir -p "$osv_dir"
-curl -Lo "$osv_dir\\osv-scanner.exe" "https://github.com/google/osv-scanner/releases/download/v$osv_version/osv-scanner_${osv_version}_windows_amd64.exe"
-echo "$osv_dir" >> "$GITHUB_PATH"
+scoop install osv-scanner
 echo "::endgroup::"
 
 echo "::group::Install Flutter"
