e9af0f80ed68c9ff8ca6d634593d437f787210d1
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-03-11 14:55:35 +0000

    ..

--- tool/ci/setup_windows.sh
+++ tool/ci/setup_windows.sh
@@ -3,9 +3,9 @@ set -exo pipefail
 
 echo "::group::Install OSV-Scanner"
 osv_version=$(git ls-remote --tags https://github.com/google/osv-scanner.git | grep -v "-" | cut -d 'v' -f2 | tail -n1)
-osv_dir=$RUNNER_TOOL_CACHE/osv-scanner
+osv_dir=$RUNNER_TOOL_CACHE\\osv-scanner
 mkdir -p "$osv_dir"
-curl -Lo "$osv_dir/osv-scanner.exe" "https://github.com/google/osv-scanner/releases/download/v$osv_version/osv-scanner_${osv_version}_windows_amd64.exe"
+curl -Lo "$osv_dir\\osv-scanner.exe" "https://github.com/google/osv-scanner/releases/download/v$osv_version/osv-scanner_${osv_version}_windows_amd64.exe"
 echo "$osv_dir" >> "$GITHUB_PATH"
 echo "::endgroup::"
 
